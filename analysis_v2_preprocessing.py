d7184970a9c80d30720c4448d89759836f3b7cbb
rickchung <rickjung88@gmail.com>
2020-01-17 15:32:21 -0700

    Update analysis_v2_preprocessing.py
    - Update the rule of locating event windows
    - Reorganize and refactor the code
    - Mark vis_types and normalized time offsets in code snapshots
    - Find differences in code snapshots

--- analysis_v2_preprocessing.py
+++ analysis_v2_preprocessing.py
@@ -1,47 +1,56 @@
 """
 This script contains the code migrated from "analysis_v2_model.ipynb". It is aimed to tidy up the notebook by removing some preprocessing parts.
+
+Output Files
+
+    state_stimes.csv:
+
+        A timetalbe of identified states based on the task log. The data are recognized by the "user_id" because records on the same device does not incur the time sync problem.
+
+        This is an intermediate file and you probably don't need this.
+
+    event_windows.csv:
+
+        A timetable containing event windows by the rule described above.
+
+        This is an intermediate file and you probably don't need this.
+
+    merged_code_preexec.csv:
+
+        A timetable of all pre-exec code for groups (group_id). Since the code has been merged (i.e., code on two devices are merged and aligned), the data are recognized by group_id. The user_id is actually irrelevant in this table.
+
+    merged_code_postedit.csv
+
+        A time table of all post-edit code snapshots. This table should be interpreted on a user-by-user basis because there may be only one user modified his/her code at times. The code snapshots of P1 and P2 are not merged because it is not easy to align code by following the current version of task log.
+
+    trans_records_v0.csv:
+
+        This file appends event windows to the transaction records coming along with some preprocessed things.
+
+
 """
 
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import pandas as pd
+import numpy as np
 import seaborn as sns
 import re
+import difflib
+from collections import Counter
 from pprint import pprint
 
-# Load the data
-print("[INFO] Loading input files")
-df_trans = pd.read_csv('data.v1.csv')
-df_code = pd.read_csv('code_snapshot_records.csv')
-df_tasklog = pd.read_csv('task_log_records.csv')
 
-# Correct the data type of timestamp
-df_trans['timestamp'] = pd.to_datetime(df_trans['timestamp'])
-df_code['timestamp'] = pd.to_datetime(df_code['timestamp'])
-df_tasklog['timestamp'] = pd.to_datetime(df_tasklog['timestamp'])
+def pair_code_snapshots(dfgp_code):
+    """
+    Pair and merge code in the dataframe dfgp_code.
 
-
-# ============================================================
-# Preprocess the task log
-print("[INFO] Processing task log data")
-df_tasklog.sort_values('timestamp', inplace=True)
-
-
-# ============================================================
-# Preprocess the code
-print("[INFO] Processing code snapshots")
-
-# Replace code linebreaks by whitespace
-df_code['snapshot'] = df_code['snapshot'].str.replace('\n', ' ')
-
-df_code_postedit = df_code[df_code['category'] == 'PostEdit']  # Fine-grained
-df_code_preexec = df_code[df_code['category'] == 'PreExec']
-
-# Pair and join the code
-
-
-def pair_code_snapshots(code):
-    x = code.copy()
+    Returns:
+        - The user ID of the first player
+        - A series of merged code (items are strings)
+        - A series of paired code (items are 2-item tuples)
+    """
+    x = dfgp_code.copy()
     x.sort_values('timestamp', inplace=True)
     (u1_id, u1_code), (u2_id, u2_code) = x.groupby('user_id')
 
@@ -71,15 +80,170 @@ def pair_code_snapshots(code):
     return p1_id, merged_code, paired_code
 
 
-df_mc_preexec = []
-for _, df_code_gp in df_code_preexec.groupby('group_id'):
-    p1_id, sample_mc, sample_pc = pair_code_snapshots(df_code_gp)
-    df_code_gp1 = df_code_gp[df_code_gp['user_id']
-                             == p1_id].copy().reset_index()
-    df_code_gp1['snapshot'] = sample_mc
-    df_mc_preexec.append(df_code_gp1)
-df_mc_preexec = pd.concat(df_mc_preexec).reset_index().drop(
-    ['level_0', 'index'], axis=1)
+def gen_event_win_record(row, event_win):
+    """
+    A helper function for generating a record of an event window.
+    """
+    return {
+        'user_id': row['user_id'],
+        'event_win': event_win,
+        'time_until': row['timestamp'],
+    }
+
+
+def annotate_event_wins(df, event_windows, heuristic=True):
+    """
+    Iterate df and annotate event windows on rows by using timestamps in event_windows.
+    """
+    trans_event = []
+    for i, trans in df.iterrows():
+        user_id = trans['user_id']
+        timestamp = trans['timestamp']
+        user_event_wins = event_windows[event_windows['user_id'] == user_id]
+
+        # Try to find the event window covers the most time of transcription clip
+        if heuristic:
+            audio_len = trans['audio_len']
+            # Find candidate events and use the one covering the most talk time
+            e1e2 = user_event_wins[timestamp <
+                                   user_event_wins['time_until']].iloc[:2]
+            # If a trans goes over the boundary, use the last window
+            if e1e2.shape[0] == 0:
+                event = user_event_wins.iloc[-1]['event_win']
+            # If there's only one matched window, use it
+            elif e1e2.shape[0] != 2:
+                event = e1e2.iloc[0]['event_win']
+            else:
+                e1, e2 = e1e2.iloc[0], e1e2.iloc[1]
+                e1_coverage = e1['time_until'] - timestamp
+                e2_coverage = e2['time_until'] - timestamp
+                # If e2 goes beyond the coverage of trans, use e1
+                if e2_coverage.seconds > audio_len:
+                    event = e1['event_win']
+                # If e2 covers more than e1, use e2
+                elif (e2_coverage - e1_coverage).seconds > e1_coverage.seconds:
+                    event = e2['event_win']
+                else:
+                    event = e1['event_win']
+        # Just find the closest match
+        else:
+            e1 = user_event_wins[
+                timestamp < user_event_wins['time_until']].iloc[:1]
+            if e1.shape[0] == 0:
+                event = user_event_wins.iloc[-1]['event_win']
+            else:
+                event = e1.iloc[0]['event_win']
+
+        trans_event.append(event)
+    return trans_event
+
+
+def _find_code_diff(x):
+    """
+    Find changes between two code snapshots and count numbers of lines changed.
+
+    Type of code changes:
+
+        1. Tweaking parameters
+
+            - Continue_Sec (4)
+            ?               ^
+            + Continue_Sec (5)
+            ?               ^
+
+        2. Tweaking functions
+
+            - Climb (down)
+            + Hover ()
+    """
+    code_snapshots = x['snapshot']
+    def get_code(x): return [c.strip() for c in x.split('; ') if len(c) != 0]
+    differ = difflib.Differ()
+
+    shifted = code_snapshots.shift()
+    diffs_list = []
+    num_removed = []
+    num_added = []
+    num_arg_tweaked = []
+    for s1, s2 in zip(shifted.values, code_snapshots.values):
+        if s1 is np.nan or s2 is np.nan:
+            diffs_list.append(np.nan)
+
+            num_removed.append(np.nan)
+            num_added.append(np.nan)
+            num_arg_tweaked.append(np.nan)
+        else:
+            s1code, s2code = get_code(s1), get_code(s2)
+            diffs = [i for i in differ.compare(
+                s1code, s2code) if i[:2] != '  ']
+            diffs_list.append(diffs)
+
+            c = Counter([i[0] for i in diffs])
+            num_removed.append(c['-'])
+            num_added.append(c['+'])
+            num_arg_tweaked.append(c['?'] / 2)
+
+    rt = x.copy()
+    rt['code_diff'] = diffs_list
+    rt['num_removed'] = num_removed
+    rt['num_added'] = num_added
+    rt['num_arg_tweaked'] = num_arg_tweaked
+
+    return rt
+
+
+def _mark_vis_type_code(x, timetable):
+    index_value = x.name
+    a_time = timetable.loc[index_value, 'A']
+    n_time = timetable.loc[index_value, 'N']
+
+    # Remove all snapshots saved before the first task
+    first, second = (a_time, n_time) if a_time <= n_time else (n_time, a_time)
+    rt = x[x['timestamp'] >= first].copy()
+
+    # Mark the rest of snapshots
+    rt['vis_type'] = 'Unknown'
+    if a_time < n_time:
+        # If A comes first
+        rt.loc[(rt['timestamp'] >= a_time), ['vis_type']] = 'A'
+        rt.loc[(rt['timestamp'] >= n_time), ['vis_type']] = 'N'
+    else:
+        # If N comes first
+        rt.loc[(rt['timestamp'] >= n_time), ['vis_type']] = 'N'
+        rt.loc[(rt['timestamp'] >= a_time), ['vis_type']] = 'A'
+
+    # Mark time offset
+    def _mark_time_offset(y):
+        y = y.sort_values('timestamp')
+        rt1 = y['timestamp'].diff().fillna(pd.Timedelta(seconds=0)).cumsum()
+        rt1 = rt1.apply(lambda x: x.seconds)
+        rt1 = rt1 / rt1.max()
+        return rt1
+    time_offsets = rt.groupby(['vis_type']).apply(_mark_time_offset)
+    rt['time_offset_vt'] = None
+    rt.loc[rt['vis_type'] == 'A', 'time_offset_vt'] = time_offsets.loc['A']
+    rt.loc[rt['vis_type'] == 'N', 'time_offset_vt'] = time_offsets.loc['N']
+
+    return rt
+
+
+# ============================================================
+# Load the data
+print("[INFO] Loading input files")
+df_trans = pd.read_csv('data.v1.csv')
+df_code = pd.read_csv('code_snapshot_records.csv')
+df_tasklog = pd.read_csv('task_log_records.csv')
+
+# Correct the data type of timestamp
+df_trans['timestamp'] = pd.to_datetime(df_trans['timestamp'])
+df_code['timestamp'] = pd.to_datetime(df_code['timestamp'])
+df_tasklog['timestamp'] = pd.to_datetime(df_tasklog['timestamp'])
+
+
+# ============================================================
+# Preprocess the task log
+print("[INFO] Processing task log data")
+df_tasklog.sort_values('timestamp', inplace=True)
 
 
 # ============================================================
@@ -186,20 +350,11 @@ exec_stimes.loc[
     (exec_stimes['duration'] > 5.0),
     'state'] = LOG_STATE_ERROR
 
-# Iterate states and identify event windows
-
-
-def get_win_record(row, event_win): return {
-    'user_id': row['user_id'],
-    'event_win': event_win,
-    'time_until': row['timestamp'],
-}
-
-
-# Seperate the exec_stimes table by user_id and vis_type
+# Seperate the exec_stimes table by user_id
 user_exec_stimes = [x for _, x in exec_stimes.groupby('user_id')]
 event_windows = []
 
+# Iterate states and identify event windows
 # For each user x's exec_stimes (timeline), annotate event windows
 for x in user_exec_stimes:
     prev_event_state = None
@@ -215,7 +370,8 @@ for x in user_exec_stimes:
             if (prev_event_state == LOG_STATE_EXEC and
                     row['state'] == LOG_STATE_ERROR):
 
-                event_windows.append(get_win_record(row, WIN_EVENT_H_EXEC))
+                event_windows.append(
+                    gen_event_win_record(row, WIN_EVENT_H_EXEC))
                 prev_event_state = LOG_STATE_ERROR
                 prev_event_window = WIN_EVENT_H_EXEC
 
@@ -223,79 +379,80 @@ for x in user_exec_stimes:
             elif (prev_event_state == LOG_STATE_ERROR and
                   row['state'] == LOG_STATE_EXEC):
 
-                event_windows.append(get_win_record(row, WIN_EVENT_H_ERR))
+                event_windows.append(
+                    gen_event_win_record(row, WIN_EVENT_H_ERR))
                 prev_event_state = LOG_STATE_EXEC
                 prev_event_window = WIN_EVENT_H_ERR
 
     # When reaching the end of states, mark the last event window
-    event_windows.append(get_win_record(row, prev_event_window))
+    event_windows.append(gen_event_win_record(row, prev_event_window))
 
 # Convert to dataframe and set a new index
 event_windows = pd.DataFrame(event_windows)
 
-# Annotate transcription logs by event windows
-trans_event = []
-for i, trans in df_trans1.iterrows():
-    user_id = trans['user_id']
-    timestamp = trans['timestamp']
-    audio_len = trans['audio_len']
-    user_event_wins = event_windows[event_windows['user_id'] == user_id]
 
-    # Find candidate events and use the one covering the most talk time
-    e1e2 = user_event_wins[timestamp < user_event_wins['time_until']].iloc[:2]
-    # If a trans goes over the boundary, use the last window
-    if e1e2.shape[0] == 0:
-        event = user_event_wins.iloc[-1]['event_win']
-    # If there's only one matched window, use it
-    elif e1e2.shape[0] != 2:
-        event = e1e2.iloc[0]['event_win']
-    else:
-        e1, e2 = e1e2.iloc[0], e1e2.iloc[1]
-        e1_coverage = e1['time_until'] - timestamp
-        e2_coverage = e2['time_until'] - timestamp
-        # If e2 goes beyond the coverage of trans, use e1
-        if e2_coverage.seconds > audio_len:
-            event = e1['event_win']
-        # If e2 covers more than e1, use e2
-        elif (e2_coverage - e1_coverage).seconds > e1_coverage.seconds:
-            event = e2['event_win']
-        else:
-            event = e1['event_win']
+# ============================================================
+# Preprocess the code
+print("[INFO] Processing code snapshots")
 
-    trans_event.append(event)
-df_trans1['event_win'] = trans_event
+# Replace code linebreaks by whitespace
+df_code['snapshot'] = df_code['snapshot'].str.replace('\n', ' ')
+
+# Seperate two kinds of code snapshots
+# - PostEdit: snapshots saved right after an edit
+# - PreExec: snapshots saved right before an execution
+df_code_postedit = df_code[df_code['category'] == 'PostEdit'].copy()
+df_code_preexec = df_code[df_code['category'] == 'PreExec'].copy()
+
+# Pair and join the code (PreExec Only)
+df_merged_code = []
+for _, dfgp_code in df_code_preexec.groupby('group_id'):
+    # Pair and merge code snapshots
+    p1_id, merged_code, _ = pair_code_snapshots(dfgp_code)
+    # Select the dataframe which belongs to the user
+    dfgp_code1 = dfgp_code[dfgp_code['user_id'] == p1_id]
+    dfgp_code1 = dfgp_code1.copy().reset_index()
+    # Replace the snapshots in the (copied) input dataframe
+    dfgp_code1['snapshot'] = merged_code
+    # Save for later processing
+    df_merged_code.append(dfgp_code1)
+# Concatenate all group dataframes
+df_merged_code = pd.concat(df_merged_code).reset_index()
+df_merged_code = df_merged_code.drop(['level_0', 'index'], axis=1)
+# Save for later processing
+df_code_preexec = df_merged_code.copy()
+
+# Mark vis_type and remove tutorial code
+vis_times = df_trans1.groupby(['group_id', 'vis_type'])['timestamp'].min()
+df_code_preexec1 = df_code_preexec.groupby(['group_id'])\
+    .apply(_mark_vis_type_code, timetable=vis_times)
+df_code_preexec1 = df_code_preexec1.reset_index(drop=True)
+df_code_postedit1 = df_code_postedit.groupby(['group_id'])\
+    .apply(_mark_vis_type_code, timetable=vis_times)
+df_code_postedit1 = df_code_postedit1.reset_index(drop=True)
+
+# Find differences between pre-exec code snapshots
+df_code_preexec2 = df_code_preexec1.groupby(['group_id', 'vis_type'])\
+    .apply(_find_code_diff)
+df_code_postedit2 = df_code_postedit1.groupby(['user_id', 'vis_type'])\
+    .apply(_find_code_diff)
 
 
 # ============================================================
-print("[INFO] Exporting the processed data")
-
-"""
-Export the processed data for later user
-
-state_stimes.csv:
-
-    A timetalbe of identified states based on the task log. The data are recognized by the "user_id" because records on the same device does not incur the time sync problem.
-
-    This is an intermediate file and you probably don't need this.
-
-event_windows.csv:
-
-    A timetable containing event windows by the rule described above.
-
-    This is an intermediate file and you probably don't need this.
-
-merged_code_preexec.csv:
-
-    A timetable of all pre-exec code for groups (group_id). Since the code has been merged (i.e., code on two devices are merged and aligned), the data are recognized by group_id. The user_id is actually irrelevant in this table.
-
-trans_records_v0.csv:
-
-    This file appends event windows to the transaction records coming along with some preprocessed things.
+# Annotate transcription logs by event windows
+df_trans1['event_win'] = annotate_event_wins(df_trans1, event_windows)
+# Annotate code snapshots by event windows
+df_code_preexec['event_win'] = annotate_event_wins(
+    df_code_preexec, event_windows, heuristic=False)
+df_code_postedit['event_win'] = annotate_event_wins(
+    df_code_postedit, event_windows, heuristic=False)
 
 
-"""
-
+# ============================================================
+# Save processed data
+print("[INFO] Exporting processed data")
 exec_stimes.to_csv('state_stimes.csv', index=None)
-df_mc_preexec.to_csv('merged_code_preexec.csv', index=None)
 event_windows.to_csv('event_windows.csv', index=None)
+df_code_preexec2.to_csv("code_preexec.csv", index=None)
+df_code_postedit2.to_csv("code_postedit.csv", index=None)
 df_trans1.to_csv('trans_records_v0.csv', index=None)
